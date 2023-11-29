25ff02b0dca6e9c7b6fbd59595c955086b9a0800
Elias Renman <elias.renman00@gmail.com>
2023-11-30 00:50:05 +0100

    feat: broken lazyvimand alacritty configs

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -10,7 +10,8 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    { "catppuccin/nvim", name = "catppuccin" },
+    { "folke/tokyonight.nvim" },
+    -- { "catppuccin/nvim", name = "catppuccin" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
@@ -27,7 +28,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "catppuccin-mocha" } },
+  install = { colorscheme = { "tokyonight-day" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
