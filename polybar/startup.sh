0884c0cdebb6a8060b03b3bcf29e9f2595611efb
Elias Renman <elias.renman00@gmail.com>
2024-02-01 00:03:56 +0100

    feat: i3 configs

---
+++ polybar/startup.sh
@@ -0,0 +1,7 @@
+killall -q polybar
+# Launch bar on each monitor, tray on primary
+polybar --list-monitors | while IFS=$'\n' read line; do
+	monitor=$(echo $line | cut -d':' -f1)
+	primary=$(echo $line | cut -d' ' -f3)
+	MONITOR=$monitor polybar --reload "example" &
+done
--- wlogout/readme.md
+++
@@ -1,8 +0,0 @@
-# Wlogout
-Make sure to link layout file with
-```Bash
-sudo ln ./wlogout/layout /etc/wlogout/layout
-```
-
-Icons borrowed with pride from 
-[gaesch](https://github.com/gasech/hyprland-dots/tree/main)
